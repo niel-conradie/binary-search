789edd350b18e7f9a0e56bd1c20e9b13add250f6
Niel Conradie <nielconradie93@gmail.com>
2022-06-06 14:33:24 +0200

    Create run.py
    Added run.py file to run program.

---
+++ binary-search/run.py
@@ -0,0 +1,31 @@
+import time
+
+from search import Search
+
+
+def run():
+    """ Naive and Binary Search. """
+    run = Search()
+
+    # Requesting user input.
+    user_input = run.user_input()
+    # Generating a sorted list.
+    sorted_list = run.generate_list(user_input)
+
+    # Naive search.
+    start = time.time()
+    for target in sorted_list:
+        run.naive_search(sorted_list, target)
+    end = time.time()
+    print(f"\nNaive search: {(end - start) / user_input} seconds.")
+
+    # Binary search.
+    start = time.time()
+    for target in sorted_list:
+        run.binary_search(sorted_list, target)
+    end = time.time()
+    print(f"\nBinary search: {(end - start) / user_input} seconds.")
+
+
+if __name__ == "__main__":
+    run()
