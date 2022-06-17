7fbe7fd7cd02bb225e3bfe17a8e3505c8174d72c
Niel Conradie <nielconradie93@gmail.com>
2022-06-17 12:32:41 +0200

    Update run.py
    Added restart method to run.py file.

--- binary-search/run.py
+++ binary-search/run.py
@@ -7,24 +7,30 @@ def run():
     """ Naive and Binary Search. """
     run = Search()
 
-    # Requesting user input.
-    user_input = run.user_input()
-    # Generating a sorted list.
-    sorted_list = run.generate_list(user_input)
+    while True:
+        # Requesting user input.
+        user_input = run.user_input()
+        # Generating a sorted list.
+        sorted_list = run.generate_list(user_input)
 
-    # Naive search.
-    start = time.time()
-    for target in sorted_list:
-        run.naive_search(sorted_list, target)
-    end = time.time()
-    print(f"\nNaive search: {(end - start) / user_input} seconds.")
+        # Naive search.
+        start = time.time()
+        for target in sorted_list:
+            run.naive_search(sorted_list, target)
+        end = time.time()
+        print(f"\nNaive search: {(end - start) / user_input} seconds.")
 
-    # Binary search.
-    start = time.time()
-    for target in sorted_list:
-        run.binary_search(sorted_list, target)
-    end = time.time()
-    print(f"\nBinary search: {(end - start) / user_input} seconds.")
+        # Binary search.
+        start = time.time()
+        for target in sorted_list:
+            run.binary_search(sorted_list, target)
+        end = time.time()
+        print(f"\nBinary search: {(end - start) / user_input} seconds.")
+
+        # Requesting user input.
+        run.restart()
+
+        continue
 
 
 if __name__ == "__main__":
