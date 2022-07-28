5ba983257d2bc67d0fb77d7077eb635c70ede475
Niel Conradie <nielconradie93@gmail.com>
2022-07-28 15:09:45 +0200

    Update run.py
    Removed unused old code and added start_app() method.

--- binary-search/run.py
+++ binary-search/run.py
@@ -1,36 +1,12 @@
-import time
-
 from search import Search
 
 
 def run():
-    """Naive and Binary Search."""
+    """Binary Search."""
     run = Search()
 
-    while True:
-        # Requesting user input.
-        user_input = run.user_input()
-        # Generating a sorted list.
-        sorted_list = run.generate_list(user_input)
-
-        # Naive search.
-        start = time.time()
-        for target in sorted_list:
-            run.naive_search(sorted_list, target)
-        end = time.time()
-        print(f"\nNaive search: {(end - start) / user_input} seconds.")
-
-        # Binary search.
-        start = time.time()
-        for target in sorted_list:
-            run.binary_search(sorted_list, target)
-        end = time.time()
-        print(f"\nBinary search: {(end - start) / user_input} seconds.")
-
-        # Requesting user input.
-        run.restart()
-
-        continue
+    # Starting the app.
+    run.start_app()
 
 
 if __name__ == "__main__":
